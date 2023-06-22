e1e5bfdbb14080cc61c560022934cecc72e2bb16
Natan <99294586+Natanssilva@users.noreply.github.com>
2023-06-22 00:56:26 -0300

    Delete EmporiumCoffee directory

--- EmporiumCoffee/main.js
+++
@@ -1,7 +0,0 @@
-
-const openMenu = document.querySelector('#nav-links');
-const icon = document.querySelector('#burger');
-
-icon.addEventListener('click', function clicado(){
-    openMenu.classList.toggle('is-active');
-});
