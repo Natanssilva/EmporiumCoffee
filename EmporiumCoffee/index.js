f90d27c7d6e1103b5f4a2cadab359c96ca786737
Natan <99294586+Natanssilva@users.noreply.github.com>
2023-06-23 11:38:47 -0300

    Delete EmporiumCoffee directory

--- EmporiumCoffee/index.js
+++
@@ -1,9 +0,0 @@
-// implementando o click do menu mobile
-
-const openMenu = document.querySelector('#nav-links');
-const icon = document.querySelector('#burger');
-
-icon.addEventListener('click', function clicado(){
-    openMenu.classList.toggle('is-active');
-    icon.classList.toggle('is-active')
-});
