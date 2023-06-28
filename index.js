6fb97b4590026d7fd71577921c49b9b69c1a962d
Natan <99294586+Natanssilva@users.noreply.github.com>
2023-06-27 21:04:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+ 
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+  
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+      
